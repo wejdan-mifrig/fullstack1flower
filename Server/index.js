b2b1f623a05898fc80fae764b78d4dc718103ce1
wejdan-mifrig <wejdanmifrig@gmail.com>
2026-09-12 16:39:56 +0300

    update code

--- Server/index.js
+++ Server/index.js
@@ -103,9 +103,9 @@ app.use("/api", cartRoutes);
 app.use(errorHandler);
 
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`server running on port ${port}`);
+
 });
